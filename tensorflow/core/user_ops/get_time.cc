f1a0cada4e034dbdf8f0548dee997784fb68c31f
styagi <styagi@iu.edu>
2020-02-16 21:09:52 -0500

    replacing output_tensor by output

--- tensorflow/core/user_ops/get_time.cc
+++ tensorflow/core/user_ops/get_time.cc
@@ -7,8 +7,8 @@ using namespace tensorflow;
 using namespace std::chrono;
 
 REGISTER_OP("GetTime")
-		.Input("input_loss_or_gradvars: float32")
-		.Output("out_timestamp: float32");
+		.Input("input_loss_or_gradvars: int32")
+		.Output("out_timestamp: int32");
 
 class GetTimeOp : public OpKernel {
 	public:
@@ -17,16 +17,15 @@ class GetTimeOp : public OpKernel {
   		void Compute(OpKernelContext* context) override {
   			//fetching the input tensor
   			const Tensor& input_tensor = context->input(0);
-  			auto input = input_tensor.flat<float32>();
+  			auto input = input_tensor.flat<int32>();
 
   			//creating the output tensor of dim (0)
   			Tensor* output_tensor = NULL;
   			OP_REQUIRES_OK(context, context->allocate_output(0, [], &output_tensor));
-  			auto output = output_tensor->template flat<float32>();
+  			auto output = output_tensor->template flat<int32>();
 
   			microseconds ms = duration_cast< microseconds >(system_clock::now().time_since_epoch());
-  			output_tensor = ms.count();
-  			//output_tensor(0) = ms.count();
+  			output = ms.count();
   		}
 };
 
