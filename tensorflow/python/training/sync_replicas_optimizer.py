37c823236daf31b2c045fd5d353cb4874ec182ac
sahiltyagi4 <sahilt.tyagi@gmail.com>
2020-07-21 20:23:31 -0400

    adding b_simple for calculating noise in gradients

--- tensorflow/python/training/sync_replicas_optimizer.py
+++ tensorflow/python/training/sync_replicas_optimizer.py
@@ -393,21 +393,20 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
           # gradient_variance = tf.math.reduce_variance(vars_concat)
           # var_assign = tf.assign(self._grad_variance, gradient_variance, name='variance_aggregated')
 
-        with ops.control_dependencies([var_assign]):
-          with ops.control_dependencies([b_simple_assign]):
-            with ops.control_dependencies([update_op]):
-              # Sync_op needs to insert tokens to the token queue at the end of the
-              # step so the replicas can fetch them to start the next step.
-              tokens = array_ops.fill([self._tokens_per_step], global_step)
-              sync_op = sync_token_queue.enqueue_many((tokens,))
+        with ops.control_dependencies([var_assign, b_simple_assign]):
+          with ops.control_dependencies([update_op]):
+            # Sync_op needs to insert tokens to the token queue at the end of the
+            # step so the replicas can fetch them to start the next step.
+            tokens = array_ops.fill([self._tokens_per_step], global_step)
+            sync_op = sync_token_queue.enqueue_many((tokens,))
 
-            if self._variable_averages is not None:
-              with ops.control_dependencies([sync_op]), ops.name_scope(""):
-                sync_op = self._variable_averages.apply(
-                  self._variables_to_average)
+          if self._variable_averages is not None:
+            with ops.control_dependencies([sync_op]), ops.name_scope(""):
+              sync_op = self._variable_averages.apply(
+                self._variables_to_average)
 
-            self._chief_queue_runner = queue_runner.QueueRunner(dummy_queue,
-                                                                [sync_op])
+          self._chief_queue_runner = queue_runner.QueueRunner(dummy_queue,
+                                                              [sync_op])
 
       for accum, dev in self._accumulator_list:
         with ops.device(dev):
