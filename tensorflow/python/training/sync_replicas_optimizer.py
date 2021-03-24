77aa106078e11b97960f03cf63a4e1cfc200d5c4
sahiltyagi4 <sahilt.tyagi@gmail.com>
2021-03-24 11:59:55 -0400

    fixing staleness in loss and gradients

--- tensorflow/python/training/sync_replicas_optimizer.py
+++ tensorflow/python/training/sync_replicas_optimizer.py
@@ -401,7 +401,7 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
           abc_norm = tf.math.reduce_sum(abc_flats, name='abc_norm')
           abc_assign = tf.assign(self._computed_norm, abc_norm, name='abc_norm_assign')
 
-          write_gradients_op = tf.io.write_file(os.path.join('/root/tf-master-0/', 'write_grads.txt'), abc_flats)
+          write_gradients_op = tf.io.write_file(os.path.join('/root/tf-master-0/', 'write_grads.txt'), tf.strings.as_string(abc_flats))
 
           aggregated_grads_and_vars = zip(aggregated_grad, var_list)
 
