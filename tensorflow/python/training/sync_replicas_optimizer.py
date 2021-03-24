fixing staleness in loss and gradients

tensorflow/python/training/sync_replicas_optimizer.py
@@ -401,6 +401,9 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
           abc_norm = tf.math.reduce_sum(abc_flats, name='abc_norm')
           abc_assign = tf.assign(self._computed_norm, abc_norm, name='abc_norm_assign')
 
+          grad_flatlist = [s for s in abc_flats]
+          write_gradients_op = tf.io.write_file(os.path.join('/root/tf-master-0/', 'write_grads.txt'), grad_flatlist)
+
           aggregated_grads_and_vars = zip(aggregated_grad, var_list)
 
           # sync_op will be assigned to the same device as the global step.
@@ -476,7 +479,7 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
               # clip_norm_assign = tf.assign(self._clipnorm_val, clip_norm)
 
             # with ops.control_dependencies([gradient_norm_assign, gradient_variance_assign, clip_norm_assign, compgrad_assign, abc_assign]):
-            with ops.control_dependencies([gradient_norm_assign, abc_assign]):
+            with ops.control_dependencies([gradient_norm_assign, abc_assign, write_gradients_op]):
               with ops.control_dependencies([update_op]):
                 # Sync_op needs to insert tokens to the token queue at the end of the
                 # step so the replicas can fetch them to start the next step.